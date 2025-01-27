85fd2a0535d50df56a372dd5722949e9714d6b98
Mohammed Hamad <toshiba9895@gmail.com>
2025-01-27 12:08:17 +0400

    change the theme

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,75 +1,77 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" pref>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="color-scheme" content="light">
-        <meta name="supported-color-schemes" content="light">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="color-scheme" content="light">
+    <meta name="supported-color-schemes" content="light">
 
-        <title>{{ __('Qudrat') }} | {{ $title ?? 'Page Title' }}</title>
+    <title>{{ __('Qudrat') }} | {{ $title ?? 'Page Title' }}</title>
 
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Plus+Jakarta+Sans:wght@700;800&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Plus+Jakarta+Sans:wght@700;800&display=swap"
+        rel="stylesheet">
 
-        <link href="https://fonts.googleapis.com/css2?family=Playball&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Playball&display=swap" rel="stylesheet">
 
-        <link href="https://fonts.cdnfonts.com/css/caros-soft" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/caros-soft" rel="stylesheet">
 
-        <link rel="stylesheet" href="{{ asset('assets/css/line-awesome.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/remixicon.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/line-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/remixicon.css') }}">
 
-        <link rel="stylesheet" href="{{ asset('assets/css/plugins.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/style.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/plugins.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.min.css') }}">
 
-        <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
-        />
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+    />
 
-        @vite('resources/css/app.css', 'resources/js/app.js')
-    </head>
-    <body>
-    <x-partials.nav />
+    @vite(['resources/css/app.css', 'resources/js/app.js'], 'dist')
+</head>
+<body>
+<x-partials.nav/>
 
-        {{ $slot }}
+{{ $slot }}
 
-   <x-partials.footer />
+<x-partials.footer/>
 
-        <script src="{{ asset('assets/js/plugins.min.js') }}"></script>
-        <script src="{{ asset('assets/js/flowbite.min.js') }}"></script>
-        <script src="{{ asset('assets/js/app.min.js') }}"></script>
-        <script src="{{ asset('js/jquery.min.js') }}"></script>
-    <script>
-        // Age range
-        $(document).ready(function (){
-            const rangeInput = $('.filter_age .range_input .input')
-            const progress = $('.filter_age .tow_bar_block .progress')
-            const minAge = $('.filter_age .age_min')
-            const maxAge = $('.filter_age .age_max')
-            let ageGap = 5
+<script src="{{ asset('assets/js/plugins.min.js') }}"></script>
+<script src="{{ asset('assets/js/flowbite.min.js') }}"></script>
+<script src="{{ asset('assets/js/app.min.js') }}"></script>
+<script src="{{ asset('js/jquery.min.js') }}"></script>
+<script>
+    // Age range
+    $(document).ready(function () {
+        const rangeInput = $('.filter_age .range_input .input')
+        const progress = $('.filter_age .tow_bar_block .progress')
+        const minAge = $('.filter_age .age_min')
+        const maxAge = $('.filter_age .age_max')
+        let ageGap = 5
 
-            rangeInput.on('input', function () {
-                let minValue = parseInt(rangeInput.eq(0).val())
-                let maxValue = parseInt(rangeInput.eq(1).val())
+        rangeInput.on('input', function () {
+            let minValue = parseInt(rangeInput.eq(0).val())
+            let maxValue = parseInt(rangeInput.eq(1).val())
 
-                if (maxValue - minValue <= ageGap) {
-                    if ($(this).hasClass('range_min')) {
-                        rangeInput.eq(0).val(maxValue - ageGap)
-                        minValue = maxValue - ageGap
-                    } else {
-                        rangeInput.eq(1).val(minValue + ageGap)
-                        maxValue = minValue + ageGap
-                    }
+            if (maxValue - minValue <= ageGap) {
+                if ($(this).hasClass('range_min')) {
+                    rangeInput.eq(0).val(maxValue - ageGap)
+                    minValue = maxValue - ageGap
                 } else {
-                    progress.css({
-                        'left': (minValue / rangeInput.eq(0).attr('max')) * 100 + "%",
-                        'right': 100 - (maxValue / rangeInput.eq(1).attr('max')) * 100 + "%"
-                    });
+                    rangeInput.eq(1).val(minValue + ageGap)
+                    maxValue = minValue + ageGap
                 }
+            } else {
+                progress.css({
+                    'left': (minValue / rangeInput.eq(0).attr('max')) * 100 + "%",
+                    'right': 100 - (maxValue / rangeInput.eq(1).attr('max')) * 100 + "%"
+                });
+            }
 
-                minAge.text(minValue)
-                maxAge.text(maxValue)
-            });
+            minAge.text(minValue)
+            maxAge.text(maxValue)
         });
-    </script>
-    </body>
+    });
+</script>
+</body>
 </html>
