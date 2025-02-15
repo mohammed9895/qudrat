5eb2a2f836d4fd79561fc71f4daee9cb232f5df9
Mohammed Hamad <toshiba9895@gmail.com>
2025-02-16 02:57:09 +0400

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/filament/user/theme.css'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/styles.min.css', 'resources/css/filament/user/theme.css'],
             refresh: true,
         }),
     ],
