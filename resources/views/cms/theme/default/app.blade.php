06d518b62ab93977a0c4d318b31e0ae7e54e4b81
Mohammed Hamad <toshiba9895@gmail.com>
2025-02-16 03:33:00 +0400

    buold

--- resources/views/cms/theme/default/app.blade.php
+++ resources/views/cms/theme/default/app.blade.php
@@ -42,7 +42,7 @@
     />
 
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'], 'dist')
+    @vite(['resources/css/app.css', 'resources/js/app.js'], 'build')
     @stack('styles')
 </head>
 
