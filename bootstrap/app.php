d09796f502338cf5671fb0ac03f0dcbca92a8c15
Mohammed Hamad <toshiba9895@gmail.com>
2025-05-18 12:37:18 +0400

    http

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\SessionTimeout;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->appendToGroup('web', LocaleMiddleware::class);
         $middleware->append(SessionTimeout::class);
+        $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_ALL);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
