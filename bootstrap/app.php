752be09711c4b0aff1ece7344e5e4544a996b70b
mohammed9895 <toshiba9895@gmail.com>
2025-05-25 18:02:29 +0400

    update password

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,12 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
         //  $middleware->trustProxies(at: '*');
         // $middleware->append(CorsMiddleware::class)
         $middleware->trustProxies(at: '*');
-         $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_FOR |
-                Request::HEADER_X_FORWARDED_HOST |
-                Request::HEADER_X_FORWARDED_PORT |
-                Request::HEADER_X_FORWARDED_PROTO |
-                Request::HEADER_X_FORWARDED_AWS_ELB
-            );
+        
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
