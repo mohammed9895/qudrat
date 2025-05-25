ed47e9c856b3f06fd859d928d1764c8c5068e7d5
mohammed9895 <toshiba9895@gmail.com>
2025-05-25 17:47:39 +0400

    update password

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,9 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->appendToGroup('web', LocaleMiddleware::class);
         $middleware->append(SessionTimeout::class);
-         $middleware->trustProxies(at: '*');
+        //  $middleware->trustProxies(at: '*');
         // $middleware->append(CorsMiddleware::class)
-        // $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_ALL);
+        $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_ALL);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
