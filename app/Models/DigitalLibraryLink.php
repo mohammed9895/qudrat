fee644417475ede92d798e241800322dc59df27c
mohammed9895 <toshiba9895@gmail.com>
2025-05-25 19:07:03 +0400

    update password

--- app/Models/DigitalLibraryLink.php
+++ app/Models/DigitalLibraryLink.php
@@ -5,25 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
 use Spatie\Translatable\HasTranslations;
-use Illuminate\Support\Facades\App;
 
 class DigitalLibraryLink extends Model
 {
     use HasTranslations;
 
-    public $translatable = ['title', 'description', 'cover'];
+    public $translatable = ['title', 'description'];
 
     protected $guarded = [];
 
     public function getThumbnailImage()
     {
-        // Get the current application locale (e.g., 'en' or 'ar')
-        $lang = App::getLocale();
-
-        // Check if the cover array has the language key and return the respective cover image URL
-        $coverKey = $this->cover[$lang] ?? null;
-
-        // If a cover image exists for the language, return its URL, else fallback to the default
-        return $coverKey ? Storage::disk('public')->url($coverKey) : asset('assets/images/unset.jpg');
+        return !$this->cover ? asset('assets/images/unset.jpg') : Storage::disk('public')->url($this->cover);
     }
 }
