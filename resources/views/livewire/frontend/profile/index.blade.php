f14d4df914a93c389b0776c5800d81183f7f02ce
mohammed9895 <toshiba9895@gmail.com>
2025-06-02 01:24:38 +0400

    fff

--- resources/views/livewire/frontend/profile/index.blade.php
+++ resources/views/livewire/frontend/profile/index.blade.php
@@ -92,7 +92,7 @@
                             @endif
                         </ul>
                         @if (auth()->check() && $profile->can_send_message && auth()->id() != $profile->user_id)
-                            <a wire:click="send_message"
+                            <a wire:click="send_message" wire:ignore
                                 class="px-8 py-3 mb-3 cursor-pointer rounded-full text-head-color font-medium bg-gray-200 text-gray-500 inline-flex items-center gap-2 w-full justify-center">
                                     <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
