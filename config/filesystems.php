28688c844878b0cea30b446b693c9c38704e3982
Mohammed Hamad <toshiba9895@gmail.com>
2025-07-01 14:28:13 +0400

    filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,6 +56,12 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
+        'nfs' => [
+            'driver' => 'local',
+            'root' => '/mnt/laravel_uploads',
+            'url' => env('APP_URL') . '/uploads',
+            'visibility' => 'public',
+        ],
 
     ],
 
